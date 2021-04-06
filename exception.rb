5411de739d08404d73c3ed7486d46532ca028e1c
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-04-06 22:25:40 +0900

    Add rescue

--- exception.rb
+++ exception.rb
@@ -1,2 +1,18 @@
 #例外処理
 
+puts 'Start'
+
+module Greeter
+  def hello
+    puts "hello"
+  end
+end
+
+begin
+  #moduleはインスタンスを作成できないのでGreeter.newでエラー(例外)発生
+  greeter = Greeter.new
+rescue
+  puts "例外が発生したが、このまま続行"
+end
+
+puts 'End'
