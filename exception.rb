87a0e8c527aef56e56d32d88b03a6f85a3148975
oshinkoshinko <aiueokakatyuka.9@gmail.com>
2021-04-06 22:46:43 +0900

    Add message & backtrace method

--- exception.rb
+++ exception.rb
@@ -16,3 +16,38 @@ rescue
 end
 
 puts 'End'
+
+def method_1
+  puts "method1 start"
+  begin
+    method2
+  rescue
+    puts "例外発生！"
+  end
+  puts "method1 end"
+end
+
+def method_2
+  puts "method2 start"
+    method3
+  puts "method2 end"
+end
+
+def method_3
+  puts "method3 start"
+    1 / 0
+  puts "method3 end"
+end
+
+method_1
+
+#例外オブジェクト情報取得 messageメソッド、backtraceメソッド
+begin
+  1 / 0
+rescue => e
+  puts "エラークラス: #{e.class}"
+  puts "エラーメッセージ: #{e.message}"
+  puts "バックトレース ---"
+  puts e.backtrace
+  puts "---"
+end
